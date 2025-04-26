1d7f3a09b1599777fe8876e4549d54f31ded25c9
matidr <matias.dirusso@gmail.com>
2025-04-26 01:11:10 -0300

    Add new stats to driver detail view

--- Formula1/Application/DriverDetail/View/DriverDetailSuccessView.swift
+++ Formula1/Application/DriverDetail/View/DriverDetailSuccessView.swift
@@ -11,7 +11,37 @@ struct DriverDetailSuccessView: View {
     
     var body: some View {
         VStack {
-            Text(driver.name).foregroundStyle(.white)
-        }.frame(maxWidth: .infinity, minHeight: UIScreen.main.bounds.height, maxHeight: .infinity).background(.black)
+            Text(driver.name.lowercased().capitalized).font(.system(size: 32, design: .rounded)).fontWeight(.heavy).foregroundStyle(.white)
+            Text(driver.teamName.lowercased().capitalized).font(.system(size: 24, design: .rounded)).foregroundStyle(Color(hex: driver.teamColor))
+            
+            HStack(alignment: .center) {
+                Spacer()
+                if let numberOfChampionships = driver.worldChampionships {
+                    Image("cup").resizable().frame(width: 40, height: 40)
+                    Text("\(numberOfChampionships)").font(.system(size: 18, design: .rounded)).fontWeight(.bold).foregroundStyle(.yellow)
+                }
+                Spacer()
+                if let numberOfRaces = driver.races {
+                    Image("race").renderingMode(.template).resizable().frame(width: 40, height: 40).foregroundStyle(.white)
+                    Text("\(numberOfRaces)").font(.system(size: 18, design: .rounded)).fontWeight(.bold).foregroundStyle(.white)
+                }
+                Spacer()
+                if let podiums = driver.podiums {
+                    Image("podium").resizable().frame(width: 40, height: 40)
+                    Text("\(podiums)").font(.system(size: 18, design: .rounded)).fontWeight(.bold).foregroundStyle(.white)
+                }
+                Spacer()
+                if let points = driver.carreerPoints {
+                    Image("points").resizable().frame(width: 30, height: 30)
+                    Text("\(points)").font(.system(size: 18, design: .rounded)).fontWeight(.bold).foregroundStyle(.white)
+                }
+                Spacer()
+            }.padding(.top, 30)
+            Spacer()
+        }.frame(maxWidth: .infinity, maxHeight: .infinity).background(.black)
     }
+}
+
+#Preview {
+    DriverDetailSuccessView(driver: Driver(id: 44, name: "Lewis Hamilton", teamColor: "#FF0000", teamName: "Ferrari", nameAcronym: "HAM", worldChampionships: 7, races: 230, podiums: 120, carreerPoints: "430"))
 }
