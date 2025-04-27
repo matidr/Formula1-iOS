560959aa44057df3cd3272debfb4f6e7eb3f03ee
matidr <matias.dirusso@gmail.com>
2025-04-26 22:44:13 -0300

    Switch to use Firestore database instead of public APIs to have better control over the data I get

--- Formula1/Data/DataSource/DriverRemoteDataSourceImpl.swift
+++ Formula1/Data/DataSource/DriverRemoteDataSourceImpl.swift
@@ -7,72 +7,37 @@
 
 import Foundation
 import Combine
+import FirebaseFirestore
 
 struct DriverRemoteDataSourceImpl: DriversDataSource {
+    let db = Firestore.firestore()
     
     func getDrivers() async throws -> [Driver] {
-        return try await makeDriversCall(driverNumber: nil)
+        do {
+            let querySnapshot = try await db.collection("drivers").getDocuments()
+            let drivers: [Driver] = try querySnapshot.documents.map { document in
+                let dto = try document.data(as: DriverDto.self)
+                return Driver(
+                    id: dto.driverNumber ?? 0,
+                    name: dto.name ?? "",
+                    imageUrl: URL(string: dto.image ?? ""),
+                    teamColor: dto.teamColor ?? "",
+                    teamName: dto.teamName ?? "",
+                    nameAcronym: dto.nameAcronym ?? ""
+                )
+            }
+            return drivers
+        } catch {
+            throw error
+        }
     }
     
     func getDriver(driverNumber: Int, name: String) async throws -> Driver {
-        async let basicDriverInfo = makeDriversCall(driverNumber: driverNumber).first
-        async let extraDriverInfo = makeDriverExtraInfoCall(name: name)
-        guard var driver = try await basicDriverInfo else { throw SimpleError.wrongInfo }
-        guard let extraDriverInfoResponse = try await extraDriverInfo else { throw SimpleError.wrongInfo }
-        driver.birthDate = extraDriverInfoResponse.birthDate
-        driver.carreerPoints = extraDriverInfoResponse.carreerPoints
-        driver.countryName = extraDriverInfoResponse.country?.name
-        driver.countryCode = extraDriverInfoResponse.country?.code
-        driver.podiums = extraDriverInfoResponse.podiums
-        driver.races = extraDriverInfoResponse.races
-        driver.worldChampionships = extraDriverInfoResponse.worldChampionships
-        driver.teamLogo = extraDriverInfoResponse.teams?.first?.team.logo
-
-        return driver
-    }
-    
-    private func makeDriverExtraInfoCall(name: String) async throws -> DriverDetailDto? {
-        guard let url = URL(string: "https://api-formula-1.p.rapidapi.com/drivers?name=\(name)") else {
-            throw URLError(.badURL)
-        }
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        request.setValue(API_KEY, forHTTPHeaderField: "x-rapidapi-key")
-        request.setValue("api-formula-1.p.rapidapi.com", forHTTPHeaderField: "x-rapidapi-host")
-        
-        let (data, _) = try await URLSession.shared.data(for: request)
-        let decodedData = try JSONDecoder().decode(GenericResponse<[DriverDetailDto]>.self, from: data)
-        return decodedData.response.first
-    }
-    
-    private func makeDriversCall(driverNumber: Int?) async throws -> [Driver] {
-        guard let url = URL(string: "https://api.openf1.org/v1/drivers?session_key=10022" + (driverNumber.map { "&driver_number=\($0)" } ?? "")) else {
-            throw URLError(.badURL)
-        }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        
-        let (data, _) = try await URLSession.shared.data(for: request)
-        
-        let decodedData = try JSONDecoder().decode([DriverDto].self, from: data)
-        
-        return decodedData.map { dto in
-            Driver(
-                id: dto.id ?? 0,
-                name: dto.name ?? "",
-                imageUrl: URL(string: dto.image ?? ""),
-                teamColor: dto.teamColor ?? "",
-                teamName: dto.teamName ?? "",
-                nameAcronym: dto.nameAcronym ?? ""
-            )
-        }
+        throw SimpleError.notImplemented
     }
     
     enum SimpleError: Error {
         case notImplemented
         case wrongInfo
     }
-    
-    let API_KEY = "PASTE_API_KEY_HERE"
 }
--- Formula1/Data/Dto/DriverDto.swift
+++ Formula1/Data/Dto/DriverDto.swift
@@ -5,19 +5,11 @@
 //  Created by Matias Di Russo on 4/20/25.
 //
 struct DriverDto: Decodable {
-    var id: Int?
+    var documentId: String?
+    var driverNumber: Int?
     var name: String?
     var image: String?
     var teamColor: String?
     var teamName: String?
     var nameAcronym: String?
-    
-    enum CodingKeys: String, CodingKey {
-        case id = "driver_number"
-        case name = "full_name"
-        case image = "headshot_url"
-        case teamColor = "team_colour"
-        case teamName = "team_name"
-        case nameAcronym = "name_acronym"
-    }
 }
